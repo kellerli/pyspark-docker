0b28478ad1751d10c8080819bbefd112ea91cd14
Thuong Dinh <thuong.dinhhoang@asnet.com.vn>
2016-03-04 16:30:24 +0700

    runable version of the demo

--- engine.py
+++ engine.py
@@ -91,14 +91,16 @@ class RecommendationEngine:
 
         # Load ratings data for later use
         logger.info("Loading Ratings data...")
-        ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
+        # ratings_file_path = os.path.join(dataset_path, 'ratings.csv')
+        ratings_file_path = "s3n://pyspark-demo/ml-latest-small/ratings.csv"
         ratings_raw_RDD = self.sc.textFile(ratings_file_path)
         ratings_raw_data_header = ratings_raw_RDD.take(1)[0]
         self.ratings_RDD = ratings_raw_RDD.filter(lambda line: line!=ratings_raw_data_header)\
             .map(lambda line: line.split(",")).map(lambda tokens: (int(tokens[0]),int(tokens[1]),float(tokens[2]))).cache()
         # Load movies data for later use
         logger.info("Loading Movies data...")
-        movies_file_path = os.path.join(dataset_path, 'movies.csv')
+        # movies_file_path = os.path.join(dataset_path, 'movies.csv')
+        movies_file_path = "s3n://pyspark-demo/ml-latest-small/movies.csv"
         movies_raw_RDD = self.sc.textFile(movies_file_path)
         movies_raw_data_header = movies_raw_RDD.take(1)[0]
         self.movies_RDD = movies_raw_RDD.filter(lambda line: line!=movies_raw_data_header)\
@@ -112,4 +114,4 @@ class RecommendationEngine:
         self.seed = 5L
         self.iterations = 10
         self.regularization_parameter = 0.1
-        self.__train_model() 
+        self.__train_model()
